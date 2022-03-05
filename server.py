47c2a98a8107ec9bd1f3d197f2945312d6aa91e6
tbjep <tbjepsen1@gmail.com>
2022-03-05 23:12:40 +0100

    Return some data to client

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
-from socketserver import UDPServer, BaseRequestHandler
+from socketserver import BaseRequestHandler, DatagramRequestHandler, UDPServer
 
-class MyHandler(BaseRequestHandler):
+class MyHandler(DatagramRequestHandler):
     def handle(self):
-        self.data = self.request[0]
-        print(self.data)
+        data = self.request[0]
+        socket = self.request[1]
+        print(data)
+        print(socket)
+        socket.sendto(data.upper(), self.client_address)
 
 def main():
     with UDPServer(('127.0.0.1', 3333), MyHandler) as server:
